41fa4c144ec6eebb9261c57485f8ad98298da98e
Xinlong Du <xinlongdu@berkeley.edu>
2024-09-04 14:14:08 -0700

    Include longer duration and compare std and peak of Cp

--- postProcessing/validation.py
+++ postProcessing/validation.py
@@ -21,7 +21,9 @@ tpuData=scipy.io.loadmat('./TPU_data/Cp_ts_g12042290.mat');
 tpuCp=tpuData['Wind_pressure_coefficients'];
 
 #load LES data
-lesData=np.loadtxt('./LES_data/p');
+lesData1=np.loadtxt('./LES_data/0/p');
+lesData2=np.loadtxt('./LES_data/7.6/p');
+lesData=np.vstack((lesData1[0:3800,:],lesData2));
 lesP=lesData[1:,1:];
 time=lesData[1:,0];
 
@@ -69,6 +71,68 @@ ax.set_ylabel('Mean Cp from TPU',fontsize=fig_font_size)
 plt.xlim(-1.25,1);
 plt.ylim(-1.25,1);
 plt.axis('equal')
+
+#%% compare TPU and LES data: std
+stdTpuCp=np.std(tpuCp[0:len(time),:],axis=0);
+stdLesCp=np.std(lesCp,axis=0);
+
+fig=plt.figure(figsize=big_fig_size)
+ax = fig.add_axes([0, 0, 1, 1])
+lesPlot,=ax.plot(tapID,stdLesCp,linewidth=plt_line_width)
+tpuPlot,=ax.plot(tapID,stdTpuCp,linewidth=plt_line_width)
+plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
+ax.tick_params(direction="in")
+ax.set_xlabel('Tap ID',fontsize=fig_font_size)
+ax.set_ylabel('Std of Cp',fontsize=fig_font_size)
+ax.legend([lesPlot,tpuPlot],['LES','TPU'],prop={'size': fig_font_size})
+
+fig=plt.figure(figsize=sml_fig_size)
+ax = fig.add_axes([0, 0, 1, 1])
+ax.scatter(stdLesCp,stdTpuCp,marker='.',s=25)
+ax.plot([0.1,0.6],[0.1,0.6],'r-');
+plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
+ax.tick_params(direction="in")
+ax.set_xlabel('Std of Cp from LES',fontsize=fig_font_size)
+ax.set_ylabel('Std of Cp from TPU',fontsize=fig_font_size)
+plt.xlim(0.1,0.6);
+plt.ylim(0.1,0.6);
+plt.axis('equal')
+
+#%% compare TPU and LES data: peak
+maxTpuCp=np.max(tpuCp[0:len(time),:],axis=0);
+maxLesCp=np.max(lesCp,axis=0);
+minTpuCp=np.min(tpuCp[0:len(time),:],axis=0);
+minLesCp=np.min(lesCp,axis=0);
+
+fig=plt.figure(figsize=big_fig_size)
+ax = fig.add_axes([0, 0, 1, 1])
+maxLesPlot,=ax.plot(tapID,maxLesCp,linewidth=plt_line_width)
+maxTpuPlot,=ax.plot(tapID,maxTpuCp,linewidth=plt_line_width)
+minLesPlot,=ax.plot(tapID,minLesCp,linewidth=plt_line_width)
+minTpuPlot,=ax.plot(tapID,minTpuCp,linewidth=plt_line_width)
+plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
+ax.tick_params(direction="in")
+ax.set_xlabel('Tap ID',fontsize=fig_font_size)
+ax.set_ylabel('Max of Cp',fontsize=fig_font_size)
+ax.legend([maxLesPlot,maxTpuPlot,minLesPlot,minTpuPlot],['LES max','TPU max','LES min','TPU min'],prop={'size': fig_font_size})
+
+fig=plt.figure(figsize=sml_fig_size)
+ax = fig.add_axes([0, 0, 1, 1])
+ax.scatter(maxLesCp,maxTpuCp,marker='.',s=25)
+ax.scatter(minLesCp,minTpuCp,marker='.',s=25)
+ax.plot([-10,5],[-10,5],'r-');
+plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
+ax.tick_params(direction="in")
+ax.set_xlabel('Max/min of Cp from LES',fontsize=fig_font_size)
+ax.set_ylabel('Max/min of Cp from TPU',fontsize=fig_font_size)
+plt.xlim(-10,5);
+plt.ylim(-10,5);
+plt.axis('equal')
+ax.legend(['Max','Min'],prop={'size': fig_font_size})
 
 #%% compare TPU and LES data: histogram
 for i in range(0,9):
