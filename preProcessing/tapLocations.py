9bd9b8182f801f1e178fcaf4ddde85861bf00ab6
Xinlong Du <xinlongdu@berkeley.edu>
2024-09-06 16:49:37 -0700

    Calculate tap locations for the flat-roof house

---
+++ preProcessing/tapLocations.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 14:30:45 2024
+
+@author: xinlo
+"""
+
+import scipy.io
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+big_fig_size = (6,3);
+sml_fig_size = (3,3);
+plt_line_width = 0.5; 
+fig_font_size = 8;
+
+#load TPU wind tunnel test data
+tpuData=scipy.io.loadmat('../postprocessing/TPU_data/Cp_ts_g12060000.mat');
+tapCoord=tpuData['Location_of_measured_points'];
+tapX=tapCoord[0,:];
+tapY=tapCoord[1,:];
+
+#plot coordinates of pressure taps
+fig=plt.figure(figsize=sml_fig_size)
+ax = fig.add_axes([0, 0, 1, 1])
+ax.scatter(tapX,tapY,marker='.',s=25)
+plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
+ax.tick_params(direction="in")
+ax.set_xlabel('X (m)',fontsize=fig_font_size)
+ax.set_ylabel('Y (m)',fontsize=fig_font_size)
+plt.axis('equal')
+
+#%% calculate 3D coordinates for pressure taps
+tapXYZface5=np.stack((tapX[0:96],tapY[0:96],np.full((96,),12.0)),axis=1);
+
+tapXface1=tapX[96:126];
+tapYface1=tapY[96:126];
+tapXYZface1=np.stack((np.full((30,),-12.0),tapYface1,tapXface1+34),axis=1);
+
+tapXface2=tapX[126:168];
+tapYface2=tapY[126:168];
+tapXYZface2=np.stack((tapXface2,np.full((42,),-8.0),tapYface2+24),axis=1);
+
+tapXface3=tapX[168:198];
+tapYface3=tapY[168:198];
+tapXYZface3=np.stack((np.full((30,),12.0),tapYface3,34-tapXface3),axis=1);
+
+tapXface4=tapX[198:];
+tapYface4=tapY[198:];
+tapXYZface4=np.stack((tapXface4,np.full((42,),8.0),24-tapYface4),axis=1);
