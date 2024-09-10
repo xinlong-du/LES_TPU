cb0adc33e6d93b1ba8d80aed4736b867a86cec35
Xinlong Du <xinlongdu@berkeley.edu>
2024-09-09 22:54:38 -0700

    Use more data for the gable roof and change line color

--- postProcessing/validation.py
+++ postProcessing/validation.py
@@ -17,17 +17,18 @@ fig_font_size = 8;
 
 #%% load data
 #load TPU wind tunnel test data
-#tpuData=scipy.io.loadmat('./TPU_data/Cp_ts_g12042290.mat'); #gable
+tpuData=scipy.io.loadmat('./TPU_data/Cp_ts_g12042290.mat'); #gable
 #tpuData=scipy.io.loadmat('./TPU_data/Cp_ts_g12060000.mat'); #flat
-tpuData=scipy.io.loadmat('./TPU_data/Cp_ts_h12064500.mat');  #hip
+#tpuData=scipy.io.loadmat('./TPU_data/Cp_ts_h12064500.mat');  #hip
 
 tpuCp=tpuData['Wind_pressure_coefficients'];
 
 #load LES data
 #gable
-# lesData1=np.loadtxt('./LES_data/Gable/0/p');
-# lesData2=np.loadtxt('./LES_data/Gable/7.6/p');
-# lesData=np.vstack((lesData1[0:3800,:],lesData2));
+lesData1=np.loadtxt('./LES_data/Gable/0/p');
+lesData2=np.loadtxt('./LES_data/Gable/7.6/p');
+lesData3=np.loadtxt('./LES_data/Gable/17.1/p');
+lesData=np.vstack((lesData1[0:3800,:],lesData2[0:4750,:],lesData3));
 
 #flat
 # lesData1=np.loadtxt('./LES_data/Flat/0/p');
@@ -35,7 +36,7 @@ tpuCp=tpuData['Wind_pressure_coefficients'];
 # lesData=np.vstack((lesData1[0:5700,:],lesData2));
 
 #hip
-lesData=np.loadtxt('./LES_data/Hip/p');
+#lesData=np.loadtxt('./LES_data/Hip/p');
 
 lesP=lesData[501:,1:];
 time=lesData[1:9001,0];
@@ -63,8 +64,8 @@ tapID=np.arange(np.size(tpuCp,1));
 
 fig=plt.figure(figsize=big_fig_size)
 ax = fig.add_axes([0, 0, 1, 1])
-lesPlot,=ax.plot(tapID,meanLesCp,linewidth=plt_line_width)
-tpuPlot,=ax.plot(tapID,meanTpuCp,linewidth=plt_line_width)
+lesPlot,=ax.plot(tapID,meanLesCp,linewidth=plt_line_width,color="k")
+tpuPlot,=ax.plot(tapID,meanTpuCp,linewidth=plt_line_width,color="r")
 plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
 ax.tick_params(direction="in")
@@ -91,8 +92,8 @@ stdLesCp=np.std(lesCp,axis=0);
 
 fig=plt.figure(figsize=big_fig_size)
 ax = fig.add_axes([0, 0, 1, 1])
-lesPlot,=ax.plot(tapID,stdLesCp,linewidth=plt_line_width)
-tpuPlot,=ax.plot(tapID,stdTpuCp,linewidth=plt_line_width)
+lesPlot,=ax.plot(tapID,stdLesCp,linewidth=plt_line_width,color="k")
+tpuPlot,=ax.plot(tapID,stdTpuCp,linewidth=plt_line_width,color="r")
 plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
 ax.tick_params(direction="in")
@@ -121,16 +122,16 @@ minLesCp=np.min(lesCp,axis=0);
 
 fig=plt.figure(figsize=big_fig_size)
 ax = fig.add_axes([0, 0, 1, 1])
-maxLesPlot,=ax.plot(tapID,maxLesCp,linewidth=plt_line_width)
-maxTpuPlot,=ax.plot(tapID,maxTpuCp,linewidth=plt_line_width)
-minLesPlot,=ax.plot(tapID,minLesCp,linewidth=plt_line_width)
-minTpuPlot,=ax.plot(tapID,minTpuCp,linewidth=plt_line_width)
+maxLesPlot,=ax.plot(tapID,maxLesCp,linewidth=plt_line_width,color="k")
+maxTpuPlot,=ax.plot(tapID,maxTpuCp,linewidth=plt_line_width,color="r")
+minLesPlot,=ax.plot(tapID,minLesCp,linewidth=plt_line_width,color="k")
+minTpuPlot,=ax.plot(tapID,minTpuCp,linewidth=plt_line_width,color="r")
 plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
 ax.tick_params(direction="in")
 ax.set_xlabel('Tap ID',fontsize=fig_font_size)
 ax.set_ylabel('Cp',fontsize=fig_font_size)
-ax.legend([maxLesPlot,maxTpuPlot,minLesPlot,minTpuPlot],['LES max','TPU max','LES min','TPU min'],prop={'size': fig_font_size})
+ax.legend([maxLesPlot,maxTpuPlot],['LES max (min)','TPU max (min)'],prop={'size': fig_font_size})
 
 fig=plt.figure(figsize=sml_fig_size)
 ax = fig.add_axes([0, 0, 1, 1])
