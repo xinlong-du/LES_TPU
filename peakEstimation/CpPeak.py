20565ed85de1a62498bb05f4c2faf2fe37da5067
Xinlong Du <xinlongdu@berkeley.edu>
2024-10-30 17:08:36 -0700

    Save comparison of mean Cp and error intervals

--- peakEstimation/CpPeak.py
+++ peakEstimation/CpPeak.py
@@ -22,40 +22,35 @@ dirs=['0','15','30','45','60','75','90'];
 for dir1 in dirs:
     CpLES_DF=pd.read_csv('../postProcessing/LES_data/Cp/lesCpF'+dir1+'.csv',header=None);
     CpLES=CpLES_DF.to_numpy().transpose();
-    maxCpLES, minCpLES, max_std, min_std=maxminest(CpLES,dur_ratio,plot_on);
-    peakCpLES=np.hstack((maxCpLES,minCpLES));
-    np.savetxt("../postProcessing/LES_data/Cp/lesCpF"+dir1+"peak.csv",peakCpLES,fmt='%2.6f',delimiter=",");
+    meanCpLES=np.mean(CpLES,axis=1);
     
     if dir1=='0':
         CpTPU_DT=scipy.io.loadmat('../postProcessing/TPU_data/Cp_ts_g120600'+dir1+'0.mat'); 
     else:
         CpTPU_DT=scipy.io.loadmat('../postProcessing/TPU_data/Cp_ts_g120600'+dir1+'.mat');
     CpTPU=CpTPU_DT['Wind_pressure_coefficients'].transpose();
-    maxCpTPU, minCpTPU, max_std, min_std=maxminest(CpTPU,dur_ratio,plot_on);
-    peakCpTPU=np.hstack((maxCpTPU,minCpTPU));
-    np.savetxt("../postProcessing/LES_data/Cp/tpuCpF"+dir1+"peak.csv",peakCpTPU,fmt='%2.6f',delimiter=",");
+    meanCpTPU=np.mean(CpTPU,axis=1);
     
     fig=plt.figure(figsize=sml_fig_size)
     ax = fig.add_axes([0, 0, 1, 1])
-    ax.scatter(maxCpLES,maxCpTPU,marker='.',s=10,label='Max')
-    ax.scatter(minCpLES,minCpTPU,marker='.',s=10,label='Min')
-    ax.plot([-6,6],[-6,6],'k-');
-    ax.plot([-6,6],[-6/1.1,6/1.1],'b-',label='+/-10%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.9,6/0.9],'b-',linewidth=0.5);
-    ax.plot([-6,6],[-6/1.2,6/1.2],'r-',label='+/-20%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.8,6/0.8],'r-',linewidth=0.5);
-    ax.plot([-6,6],[-6/1.3,6/1.3],'m-',label='+/-30%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.7,6/0.7],'m-',linewidth=0.5);
+    ax.scatter(meanCpLES,meanCpTPU,marker='.',s=10,label='Max')
+    ax.plot([-2,2],[-2,2],'k-');
+    ax.plot([-2,2],[-2/1.1,2/1.1],'b-',label='+/-10%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.9,2/0.9],'b-',linewidth=0.5);
+    ax.plot([-2,2],[-2/1.2,2/1.2],'r-',label='+/-20%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.8,2/0.8],'r-',linewidth=0.5);
+    ax.plot([-2,2],[-2/1.3,2/1.3],'m-',label='+/-30%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.7,2/0.7],'m-',linewidth=0.5);
     plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
     plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
     ax.tick_params(direction="in")
-    ax.set_xlabel('Max/min of Cp from LES',fontsize=fig_font_size)
-    ax.set_ylabel('Max/min of Cp from TPU',fontsize=fig_font_size)
+    ax.set_xlabel('Mean Cp from LES',fontsize=fig_font_size)
+    ax.set_ylabel('Mean Cp from TPU',fontsize=fig_font_size)
     # plt.xlim(-10,6);
     # plt.ylim(-10,6);
     plt.axis('equal')
     plt.legend(prop={'size': fig_font_size})
-    figName='../postProcessing/LES_data/Cp/CompPeakCpF'+dir1+'.tif'
+    figName='../postProcessing/LES_data/Cp/CompMeanCpF'+dir1+'.tif'
     plt.savefig(figName, transparent=False, bbox_inches='tight', dpi=100)
     plt.show()
     
@@ -63,40 +58,35 @@ for dir1 in dirs:
 for dir1 in dirs:
     CpLES_DF=pd.read_csv('../postProcessing/LES_data/Cp/lesCpG'+dir1+'.csv',header=None);
     CpLES=CpLES_DF.to_numpy().transpose();
-    maxCpLES, minCpLES, max_std, min_std=maxminest(CpLES,dur_ratio,plot_on);
-    peakCpLES=np.hstack((maxCpLES,minCpLES));
-    np.savetxt("../postProcessing/LES_data/Cp/lesCpG"+dir1+"peak.csv",peakCpLES,fmt='%2.6f',delimiter=",");
+    meanCpLES=np.mean(CpLES,axis=1);
     
     if dir1=='0':
         CpTPU_DT=scipy.io.loadmat('../postProcessing/TPU_data/Cp_ts_g120422'+dir1+'0.mat'); 
     else:
         CpTPU_DT=scipy.io.loadmat('../postProcessing/TPU_data/Cp_ts_g120422'+dir1+'.mat');
     CpTPU=CpTPU_DT['Wind_pressure_coefficients'].transpose();
-    maxCpTPU, minCpTPU, max_std, min_std=maxminest(CpTPU,dur_ratio,plot_on);
-    peakCpTPU=np.hstack((maxCpTPU,minCpTPU));
-    np.savetxt("../postProcessing/LES_data/Cp/tpuCpG"+dir1+"peak.csv",peakCpTPU,fmt='%2.6f',delimiter=",");
+    meanCpTPU=np.mean(CpTPU,axis=1);
     
     fig=plt.figure(figsize=sml_fig_size)
     ax = fig.add_axes([0, 0, 1, 1])
-    ax.scatter(maxCpLES,maxCpTPU,marker='.',s=10,label='Max')
-    ax.scatter(minCpLES,minCpTPU,marker='.',s=10,label='Min')
-    ax.plot([-6,6],[-6,6],'k-');
-    ax.plot([-6,6],[-6/1.1,6/1.1],'b-',label='+/-10%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.9,6/0.9],'b-',linewidth=0.5);
-    ax.plot([-6,6],[-6/1.2,6/1.2],'r-',label='+/-20%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.8,6/0.8],'r-',linewidth=0.5);
-    ax.plot([-6,6],[-6/1.3,6/1.3],'m-',label='+/-30%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.7,6/0.7],'m-',linewidth=0.5);
+    ax.scatter(meanCpLES,meanCpTPU,marker='.',s=10,label='Max')
+    ax.plot([-2,2],[-2,2],'k-');
+    ax.plot([-2,2],[-2/1.1,2/1.1],'b-',label='+/-10%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.9,2/0.9],'b-',linewidth=0.5);
+    ax.plot([-2,2],[-2/1.2,2/1.2],'r-',label='+/-20%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.8,2/0.8],'r-',linewidth=0.5);
+    ax.plot([-2,2],[-2/1.3,2/1.3],'m-',label='+/-30%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.7,2/0.7],'m-',linewidth=0.5);
     plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
     plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
     ax.tick_params(direction="in")
-    ax.set_xlabel('Max/min of Cp from LES',fontsize=fig_font_size)
-    ax.set_ylabel('Max/min of Cp from TPU',fontsize=fig_font_size)
+    ax.set_xlabel('Mean Cp from LES',fontsize=fig_font_size)
+    ax.set_ylabel('Mean Cp from TPU',fontsize=fig_font_size)
     # plt.xlim(-10,6);
     # plt.ylim(-10,6);
     plt.axis('equal')
     plt.legend(prop={'size': fig_font_size})
-    figName='../postProcessing/LES_data/Cp/CompPeakCpG'+dir1+'.tif'
+    figName='../postProcessing/LES_data/Cp/CompMeanCpG'+dir1+'.tif'
     plt.savefig(figName, transparent=False, bbox_inches='tight', dpi=100)
     plt.show()
     
@@ -104,39 +94,34 @@ for dir1 in dirs:
 for dir1 in dirs:
     CpLES_DF=pd.read_csv('../postProcessing/LES_data/Cp/lesCpH'+dir1+'.csv',header=None);
     CpLES=CpLES_DF.to_numpy().transpose();
-    maxCpLES, minCpLES, max_std, min_std=maxminest(CpLES,dur_ratio,plot_on);
-    peakCpLES=np.hstack((maxCpLES,minCpLES));
-    np.savetxt("../postProcessing/LES_data/Cp/lesCpH"+dir1+"peak.csv",peakCpLES,fmt='%2.6f',delimiter=",");
+    meanCpLES=np.mean(CpLES,axis=1);
     
     if dir1=='0':
         CpTPU_DT=scipy.io.loadmat('../postProcessing/TPU_data/Cp_ts_h120645'+dir1+'0.mat'); 
     else:
         CpTPU_DT=scipy.io.loadmat('../postProcessing/TPU_data/Cp_ts_h120645'+dir1+'.mat');
     CpTPU=CpTPU_DT['Wind_pressure_coefficients'].transpose();
-    maxCpTPU, minCpTPU, max_std, min_std=maxminest(CpTPU,dur_ratio,plot_on);
-    peakCpTPU=np.hstack((maxCpTPU,minCpTPU));
-    np.savetxt("../postProcessing/LES_data/Cp/tpuCpH"+dir1+"peak.csv",peakCpTPU,fmt='%2.6f',delimiter=",");
+    meanCpTPU=np.mean(CpTPU,axis=1);
     
     fig=plt.figure(figsize=sml_fig_size)
     ax = fig.add_axes([0, 0, 1, 1])
-    ax.scatter(maxCpLES,maxCpTPU,marker='.',s=10,label='Max')
-    ax.scatter(minCpLES,minCpTPU,marker='.',s=10,label='Min')
-    ax.plot([-6,6],[-6,6],'k-');
-    ax.plot([-6,6],[-6/1.1,6/1.1],'b-',label='+/-10%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.9,6/0.9],'b-',linewidth=0.5);
-    ax.plot([-6,6],[-6/1.2,6/1.2],'r-',label='+/-20%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.8,6/0.8],'r-',linewidth=0.5);
-    ax.plot([-6,6],[-6/1.3,6/1.3],'m-',label='+/-30%',linewidth=0.5);
-    ax.plot([-6,6],[-6/0.7,6/0.7],'m-',linewidth=0.5);
+    ax.scatter(meanCpLES,meanCpTPU,marker='.',s=10,label='Max')
+    ax.plot([-2,2],[-2,2],'k-');
+    ax.plot([-2,2],[-2/1.1,2/1.1],'b-',label='+/-10%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.9,2/0.9],'b-',linewidth=0.5);
+    ax.plot([-2,2],[-2/1.2,2/1.2],'r-',label='+/-20%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.8,2/0.8],'r-',linewidth=0.5);
+    ax.plot([-2,2],[-2/1.3,2/1.3],'m-',label='+/-30%',linewidth=0.5);
+    ax.plot([-2,2],[-2/0.7,2/0.7],'m-',linewidth=0.5);
     plt.rc('xtick', labelsize=fig_font_size)    # fontsize of the tick labels
     plt.rc('ytick', labelsize=fig_font_size)    # fontsize of the tick labels
     ax.tick_params(direction="in")
-    ax.set_xlabel('Max/min of Cp from LES',fontsize=fig_font_size)
-    ax.set_ylabel('Max/min of Cp from TPU',fontsize=fig_font_size)
+    ax.set_xlabel('Mean Cp from LES',fontsize=fig_font_size)
+    ax.set_ylabel('Mean Cp from TPU',fontsize=fig_font_size)
     # plt.xlim(-10,6);
     # plt.ylim(-10,6);
     plt.axis('equal')
     plt.legend(prop={'size': fig_font_size})
-    figName='../postProcessing/LES_data/Cp/CompPeakCpH'+dir1+'.tif'
+    figName='../postProcessing/LES_data/Cp/CompMeanCpH'+dir1+'.tif'
     plt.savefig(figName, transparent=False, bbox_inches='tight', dpi=100)
     plt.show()
